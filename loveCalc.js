3711749d2efc031a422d28871dae4f1ef217759d
Shivananda Sai <shivanandasai.38@gmail.com>
2022-05-01 22:09:15 +0530

    Create loveCalc.js

---
+++ loveCalc.js
@@ -0,0 +1,7 @@
+var nameOne = prompt("Enter First Name :");
+var nameTwo = prompt("Eter Second Name : ");
+
+var num = Math.random();
+num = Math.floor(num *100)+1;
+
+alert("love calculator==>\n"+nameOne+" loves "+nameTwo+" : "+num+"%");
